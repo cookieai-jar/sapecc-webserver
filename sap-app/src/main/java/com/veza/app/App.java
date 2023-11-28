fc3c9efd2af45c8d582bfd44e91cf2e2169385a2
Ying Wu <ywu@veza.com>
2023-11-27 19:07:02 -0800

    Minor, remove a System.out.println

--- sap-app/src/main/java/com/veza/app/App.java
+++ sap-app/src/main/java/com/veza/app/App.java
@@ -78,7 +78,6 @@ public class App
     public static String getCurrentTimeString() {
         Date date = new Date();
         SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
-        System.out.println("Get current date time");
         return dateFormat.format(date);
     }
 
